b1b1c1310268998f5c0f6caadf6f143a4e7a7834
Kalaris Ng <kalaris3255@gmail.com>
2023-06-01 14:22:38 +0800

    change camera position

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -13,7 +13,8 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
