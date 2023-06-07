476b1c30a628e0d3379c22d1d6f2394170f39307
Kalaris Ng <kalaris3255@gmail.com>
2023-06-07 17:17:22 +0800

    add fade animation to start button

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public CanvasGroup buttonCanvasGroup;
+    public float fadeDuration = 0.3f;
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        StartCoroutine(FadeButton());
+    }
+
+    private IEnumerator FadeButton()
+    {
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, timer / fadeDuration);
+            buttonCanvasGroup.alpha = alpha;
+            yield return null;
+        }
+
+        buttonCanvasGroup.interactable = false; // Disable interactions with the button
+        buttonCanvasGroup.blocksRaycasts = false; // Disable raycast blocking on the button
+        gameObject.SetActive(false); // Disable the button game object
     }
 }
