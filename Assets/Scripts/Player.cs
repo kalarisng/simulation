d927f4d596bb9b15f746126f64dd85701e330ca6
Kalaris Ng <kalaris3255@gmail.com>
2023-06-09 02:36:53 +0800

    update player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,10 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     [Min(1)]
-    private float hitRange = 3;
+    private float hitRange = 1;
 
     private RaycastHit hit;
+    private bool canPickUp = false;
 
     // Update is called once per frame
     void Update()
@@ -27,11 +28,24 @@ public class Player : MonoBehaviour
         {
             hit.collider.GetComponent<Highlight>()?.ToggleHighlight(false);
             pickUpUI.SetActive(false);
+            canPickUp = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // call method to pick up object
+                PickUpObject(hit.collider.gameObject);
+            }
         }
         if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickableLayerMask))
         {
             hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
             pickUpUI.SetActive(true);
+            canPickUp = false;
         }
+    }
+
+    private void PickUpObject(GameObject obj)
+    {
+        // Action to take when object is picked up
+        Debug.Log("Picked up object: " + obj.name);
     }
 }
