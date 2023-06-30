fbd608d165413c1a058e4de7dec5fb306b080651
Kalaris Ng <kalaris3255@gmail.com>
2023-07-01 00:55:49 +0800

    add collision

--- Assets/Scripts/RoomDoorInteraction.cs
+++ Assets/Scripts/RoomDoorInteraction.cs
@@ -1,28 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomDoorInteraction : MonoBehaviour
 {
-    public GameObject canvasGameObject; // Reference to the canvas GameObject
+    public GameObject canvas;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the trigger collision is with the player capsule
+        Debug.Log("player collided with room door");
         if (other.CompareTag("Player"))
         {
-            // Enable the canvas
-            canvasGameObject.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        // Check if the trigger collision is with the player capsule
-        if (other.CompareTag("Player"))
-        {
-            // Disable the canvas
-            canvasGameObject.SetActive(false);
+            canvas.SetActive(true);
         }
     }
 }
