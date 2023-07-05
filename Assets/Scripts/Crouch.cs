1894baecd98c5d5df32ea184714b035d7ffff8cc
Kalaris Ng <kalaris3255@gmail.com>
2023-07-05 15:49:24 +0800

    assets & crouch

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    CharacterController characterCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterCollider = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            characterCollider.height = 1.0f;
+        }
+        else
+        {
+            characterCollider.height = 2.0f;
+        }
+    }
+}
